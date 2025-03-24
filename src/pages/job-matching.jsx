cc0cf0a1a063de0792f11cffdc076cc021eab763
abdulaziz-alfalahi <abdulaziz.harib@gmail.com>
2025-03-25 02:25:50 +0400

    Update job-matching.jsx

--- src/pages/job-matching.jsx
+++ src/pages/job-matching.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import { JobMatchingHome } from '@/components/job-matching/JobMatchingHome';
+import { Layout } from '@/components/layout';
+
+export default function JobMatchingHomePage() {
+  return (
+    <Layout>
+      <JobMatchingHome />
+    </Layout>
+  );
+}
 
