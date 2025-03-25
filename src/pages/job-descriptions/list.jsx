3d1810f2388d1066ea9956f33b3597a239ad1ff5
abdulaziz-alfalahi <abdulaziz.harib@gmail.com>
2025-03-26 02:12:06 +0400

    Update list.jsx

--- src/pages/job-descriptions/list.jsx
+++ src/pages/job-descriptions/list.jsx
@@ -1,1 +1,13 @@
+// src/pages/job-descriptions/list.jsx
+import React from 'react';
+import { JobDescriptionsList } from '@/components/job-matching/JobDescriptionsList';
+import Layout from '@/components/layout/Layout';
+
+export default function JobDescriptionsListPage() {
+  return (
+    <Layout>
+      <JobDescriptionsList />
+    </Layout>
+  );
+}
 
