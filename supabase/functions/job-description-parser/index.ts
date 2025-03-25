4874cc347460ad8049b191ad3de26f81d8124759
abdulaziz-alfalahi <abdulaziz.harib@gmail.com>
2025-03-26 03:04:30 +0400

    Update index.ts

--- supabase/functions/job-description-parser/index.ts
+++ supabase/functions/job-description-parser/index.ts
@@ -1,4 +1,4 @@
-// Updated index.ts for job-description-parser
+// supabase/functions/job-description-parser/index.ts
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
