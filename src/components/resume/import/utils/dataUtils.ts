f25c99e9b58b7956656d790987920a379a3417f4
abdulaziz-alfalahi <abdulaziz.harib@gmail.com>
2025-03-28 03:18:10 +0400

    Update dataUtils.ts

--- src/components/resume/import/utils/dataUtils.ts
+++ src/components/resume/import/utils/dataUtils.ts
@@ -1,6 +1,6 @@
-
 import { ResumeData } from '../../types';
 import { sanitizeResumeData } from '../../utils/helpers/validation';
+import { mergeResumeData as mergeResumeDataFromUtils } from '../../utils/resumeDataUtils';
 
 /**
  * Merge resume data with better validation
@@ -12,7 +12,6 @@ export const mergeResumeData = (currentData: ResumeData, parsedData: Partial<Res
   // Sanitize data before merging
   const sanitizedData = sanitizeResumeData(parsedData);
   
-  // Import and use the utility function for the actual merging
-  const { mergeResumeData: mergeResumeDataFromUtils } = require('../../utils/resumeDataUtils');
+  // Use the imported utility function for the actual merging
   return mergeResumeDataFromUtils(currentData, sanitizedData);
 };
